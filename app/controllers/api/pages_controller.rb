05cd16346521bd37d123fd56e28acd6b715d55c3
Alexandr Korsak <alex.korsak@gmail.com>
2012-07-17 23:10:42 +0300

    Add spec for json and xml responders on the list pages

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,5 +1,12 @@
 class Api::PagesController < ApplicationController
+  include ActionController::MimeResponds
+
+  respond_to :json, :xml
+
   def index
     @pages = Page.all
+
+    respond_with @pages
   end
 end
+
--- spec/controllers/api/pages_controller_spec.rb
+++ spec/controllers/api/pages_controller_spec.rb
@@ -4,11 +4,19 @@ describe Api::PagesController do
   context "list of pages" do
     let!(:about) { create(:page) }
     let!(:article) { create(:page) }
+    let(:format_type) { :json }
 
-    before { get :index, :format => :json }
+    before { get :index, :format => format_type }
 
     it { should respond_with(:success) }
     it { assigns(:pages).should == [about, article] }
+    it { response.body.should == [about, article].to_json }
+
+    context "on xml format" do
+      let(:format_type) { :xml }
+
+      it { response.body.should == [about, article].to_xml }
+    end
   end
 end
 
