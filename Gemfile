1459b4a9973904cd5c09603f7f3d01a5f328aae2
Alexandr Korsak <alex.korsak@gmail.com>
2012-07-17 22:14:04 +0300

    Initial commit of the rails-api-demo application

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.6'
+gem 'rails-api'
+gem 'heroku'
+gem 'json'
+
+group :production do
+  gem 'pg'
+end
+
+group :test do
+  gem 'mysql2'
+  gem 'rspec'
+  gem 'shoulda'
+  gem 'database_cleaner'
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'ruby_gntp'
+  gem 'guard-rspec'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bikeroar::Application.config.secret_token = '4e6ef22891d98596f3f2ca6dafc2078310b224f9e21ea6eb384d87fd8797edf4bb8ab6576d9c5a5612afd6e48ecc15cd1d8dc836bdc969e9d9010d632d36e0b4'
