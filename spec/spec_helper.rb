27829d455ca576f92684724585f4dc3418758a08
Alexandr Korsak <alex.korsak@gmail.com>
2012-07-17 22:35:38 +0300

    Include factory girl dsl methods to specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,13 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'factory_girl'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
